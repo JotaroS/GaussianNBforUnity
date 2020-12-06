4a1184b8c81145c68dc14423c847d56bb6658044
Jotaro <jojojoe77@gmail.com>
2020-12-06 21:38:08 +0100

    initial commit

---
+++ Assets/GaussianNB.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.Assertions;
+using UnityEngine.UIElements;
+
+public class GaussianNB
+{
+    Dictionary<string, float> p_class_ = new Dictionary<string, float>();
+    Dictionary<string, List<List<float>>> f_stats_ = new  Dictionary<string, List<List<float>>>();
+    List <string> labels_list_ = new List<string>();
+
+    int features_count_;
+
+    public GaussianNB(){
+
+    }
+    ~GaussianNB(){
+
+    }
+    public void Train(List <List<float>> data, List<string> labels){
+        Dictionary <string, List<List<float>>> lfm = new Dictionary <string, List<List<float>>>();
+        Dictionary <string, int> class_count = new Dictionary<string, int>();
+        int train_size = labels.Count();
+
+        labels_list_ = labels.Distinct().ToList();
+        features_count_ = data[0].Count();
+
+        foreach(var label in labels_list_){
+            class_count[label]=0;
+            List <float> arr= new float[features_count_].ToList<float>();//new List<float>(new float[features_count_]);
+            List<List<float>> tmp = new List<List<float>>();
+            tmp.Add(new float[features_count_].ToList<float>());
+            tmp.Add(new float[features_count_].ToList<float>());
+            tmp.Add(new float[features_count_].ToList<float>());
+            f_stats_.Add(label,tmp); // mean mu
+            // f_stats_.Add(label,temp); // variance2 sigma^2
+            // f_stats_.Add(label,temp); // 1/(2_PI_Var^22)
+        }
+        foreach(var label in labels_list_){
+            lfm.Add(label, new List<List<float>>());
+        }
+
+        for(int i=0; i < train_size; i++){
+            lfm[labels[i]].Add(data[i]);
+            class_count[labels[i]] += 1;
+            for(int j=0; j < features_count_; j++){
+                f_stats_[labels[i]][0][j] += data[i][j]; // E(x at label i)
+            }
+        }
+
+        foreach(var label in labels_list_){
+            for(int j=0; j < features_count_; j++){
+                f_stats_[label][0][j] /= class_count[label];
+            }
+            p_class_[label] = class_count[label] * 1.0f / train_size;
+        }
+
+        foreach(var label in labels_list_){
+            for(int j=0; j < features_count_; j++){
+                for(int i=0; i < lfm[label].Count(); i++){
+                    f_stats_[label][1][j] += Mathf.Pow(lfm[label][i][j] - f_stats_[label][0][j], 2.0f); 
+                }
+                f_stats_[label][1][j] /= class_count[label]; //biased variance!
+                Debug.Log(f_stats_[label][1][j]);
+                f_stats_[label][2][j] = 1.0f / Mathf.Sqrt(2.0f * Mathf.PI * f_stats_[label][1][j]);
+            }
+        }
+    }
+
+    public Dictionary<string, float> Predict(List<float> vec)
+    {
+        Assert.IsTrue(features_count_ == vec.Count());
+
+        Dictionary<string, float> ret = new Dictionary<string, float>();
+        
+        foreach(var label in labels_list_)
+        {
+            ret[label] = p_class_[label];
+            for(int j = 0; j < features_count_; j++)
+            {
+                ret[label] *= f_stats_[label][2][j] * Mathf.Exp(-Mathf.Pow(vec[j] - f_stats_[label][0][j], 2) / (2 * f_stats_[label][1][j]));
+            }
+        }
+        float norm = 0;
+        foreach(var label in labels_list_){
+            norm += ret[label];
+        }
+        foreach(var label in labels_list_){
+            ret[label] /= norm;
+        }
+        return ret;
+    }
+    
+};
---
+++ Assets/MyGNBTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+public class MyGNBTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    List<List<float>> train_data = new List<List<float>>();
+    List<List<float>> test_data = new List<List<float>>();
+    List<string> train_label = new List<string>();
+    List<string> test_label = new List<string>();
+
+    GaussianNB clf = new GaussianNB();
+
+    public Dictionary<string, float> pred = new Dictionary<string, float>();
+    void Start()
+    {
+        train_data = LoadData("Assets/train_states.txt");
+        train_label = LoadLabel("Assets/train_labels.txt");
+        test_data = LoadData("Assets/test_states.txt");
+        test_label = LoadLabel("Assets/test_labels.txt");
+
+        clf.Train(train_data,train_label);
+        pred = clf.Predict(test_data[0]);
+
+        foreach(var key in pred.Keys){
+            Debug.Log(pred[key]);
+        }
+        
+    }
+
+    List<List<float>> LoadData(string path){
+        List<List<float>> ret = new List<List<float>>();
+        StreamReader sr = new StreamReader(path);
+        while(sr.Peek() >= 0){
+            string s = sr.ReadLine();
+            string[] ss = s.Split(',');
+            List<float> tmp = new List<float>();
+            foreach(var s_ in ss){
+                tmp.Add(Convert.ToSingle(s_));
+            }
+            ret.Add(tmp);
+        }
+        sr.Close();
+        return ret;
+    }
+    List<string> LoadLabel(string path){
+        List<string> ret = new List<string>();
+        StreamReader sr = new StreamReader(path);
+        while(sr.Peek() >= 0){
+            string s = sr.ReadLine();
+            ret.Add(s);
+        }
+        sr.Close();
+        return ret;
+    }
+}
